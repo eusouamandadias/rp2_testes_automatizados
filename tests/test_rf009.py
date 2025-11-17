77f2f6034d1847cbb5f84fc03f0ed390e8b419e4
Amanda Dias <amandadds.aluno@unipampa.edu.br>
2025-11-16 23:12:57 -0300

    Criando um teste para automatizar a criação de slides e garantir que meus CTs relacionados aos requisitos 9 e 10 funcionem sempre

--- tests/test_rf009.py
+++ tests/test_rf009.py
@@ -5,6 +5,8 @@ Requisito: RF9 – O sistema deve permitir editar informações ou substituir sl
 """
 
 # Importando bibliotecas necessárias
+from re import A, T
+from time import time
 from playwright.sync_api import sync_playwright, expect
 
 # Importando funções utilitárias
@@ -18,6 +20,61 @@ COURSE_MANAGEMENT_URL = f"{BASE_URL}/manage-courses"
 NEW_SLIDE_TITLE = "Lição 1 - Preparando o ambiente para testar com Playwright"
 NEW_HTML_CODE = "https://docs.google.com/presentation/d/e/2PACX-1vSzcPobWyEazwXnAFux9eVqLFBMeC3yQuKbRvpgRO2OsAv34A4BJWfAXLfsDPl2vNYeUATNdVwnI4LI/pubembed?start=false&loop=false&delayms=30000"
 MESSAGE_TEXT = "O slide foi atualizado com sucesso!"
+TEMPO_DE_PAUSA = 3000
+
+# Antes de começar co o test rf009, precisamos verificar se o slide "Introdução ao Playwright" já existe e se não, criá-lo.
+def test_criando_novo_slide():
+    # Criar um contexto Playwright
+    with sync_playwright() as playwright:
+        # Inicializando o navegador
+        browser = playwright.chromium.launch(headless=False, channel=DEFAULT_BROWSER)
+
+        # Carregando credenciais de login armazenadas e criando uma nova página
+        credentials = load_credentials(playwright=playwright, browser=browser)
+        page = credentials["page"]
+
+        page.wait_for_timeout(TEMPO_DE_PAUSA)
+        # Accessar a página de gerenciamento de conteúdo do curso
+        print("\n\nIndo para a página de gerenciamento de cursos...\n")
+        page.goto(COURSE_MANAGEMENT_URL)
+        page.wait_for_timeout(TEMPO_DE_PAUSA)
+
+        card_do_curso = page.locator(".MuiCard-root").filter(
+            has_text="Testes Automatizados com Playwright"
+        )
+        page.wait_for_timeout(TEMPO_DE_PAUSA)
+
+        botao_gerenciar = card_do_curso.get_by_role("button", name="Gerenciar Curso")
+        print(
+            f"Clicando em 'Gerenciar Curso' para o curso: {'Testes Automatizados com Playwright'}"
+        )
+        page.wait_for_timeout(TEMPO_DE_PAUSA)
+        botao_gerenciar.click()
+
+        page.click('button[role="tab"]:has-text("Slides")')
+
+        page.wait_for_timeout(TEMPO_DE_PAUSA)
+        slide_card = page.locator("div.MuiPaper-root.MuiPaper-elevation2").filter(
+            has_text="Introdução ao Playwright"
+        )
+        if slide_card.count() == 0:
+            # Preenchendo o formulário para adicionar um novo slide
+            titulo_input_id = page.get_by_label("Título do Slide")
+            titulo_input_id.fill("Introdução ao Playwright")
+            page.wait_for_timeout(TEMPO_DE_PAUSA)
+
+            url_input_id = page.get_by_label("URL do Slide (Google Apresentações)")
+            url_input_id.fill(NEW_HTML_CODE)
+            page.wait_for_timeout(TEMPO_DE_PAUSA)
+
+            page.click('button:has-text("Adicionar Slide")')
+            page.wait_for_timeout(5000)
+
+        # expect(
+        #     page.locator("div.MuiPaper-root.MuiPaper-elevation2").filter(
+        #         has_text="Introdução ao Playwright"
+        #     )
+        # ).to_have_count(1)
 
 
 def test_rf009_edit_slide_success():
@@ -44,7 +101,6 @@ def test_rf009_edit_slide_success():
 
         # Acessar a aba "Slides"
         page.click('button[role="tab"]:has-text("Slides")')
-
         # Clicando no ícone "Editar" do slide "Introdução ao Playwright".
         # Encontramos esse botão pois é o primeiro na página com um svg dentro dele.
         page.click('button:has(svg[data-testid="EditIcon"])')
